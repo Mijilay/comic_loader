7d6a639d005e2593727a7e4c8e2ade8dbfbde430
Mijilay <113235115+Mijilay@users.noreply.github.com>
2024-05-14 17:33:19 +0300

    Функции разобраны на части

--- comic_loader.py
+++ comic_loader.py
@@ -19,8 +19,8 @@ def get_comic_number():
     comic_info = response.json()
     number = comic_info['num']
     return number
-def save_comic():
-    random_comic = random.randint(1, get_comic_number())
+def save_comic(random_comic):
+    
     url = f'https://xkcd.com/{random_comic}/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
@@ -28,23 +28,23 @@ def save_comic():
     png_url = comic_info['img']
     alt = comic_info['alt']
     title = comic_info['title']
-
     return png_url, alt, title 
-def upload_files(bot_api_key, chat_id):
-    png_url, alt, title = save_comic()
+def upload_files(bot_api_key, chat_id, title, alt):
     filename = f'{title}.png'
     bot = telegram.Bot(token=bot_api_key)
     with open(filename, 'rb') as file:
         bot.send_photo(chat_id=chat_id, photo=file, caption=alt)
-    os.remove(filename)
+    return filename
 
 
 
 def main():
     load_dotenv()
+    random_comic = random.randint(1, get_comic_number())
     bot_api_key = os.environ['TG_BOT_API']
     chat_id = os.environ['TG_CHAT_ID']
-    upload_files(bot_api_key, chat_id)
+    png_url, alt, title = save_comic(random_comic)
+    filename = upload_files(bot_api_key, chat_id, title, alt)
     download_comic(f'{title}.png', png_url)
     os.remove(filename)
 
