7c791c6ae3ab962e73c5033616299bf5cc45e5b5
Mijilay <113235115+Mijilay@users.noreply.github.com>
2024-05-14 17:56:51 +0300

    добавлен try finnaly

--- comic_loader.py
+++ comic_loader.py
@@ -5,12 +5,12 @@ import telegram
 from dotenv import load_dotenv
 
 
-
 def download_comic(filename, png_url):
     response = requests.get(png_url)
     response.raise_for_status()
     with open(filename, 'wb') as file:
         file.write(response.content)
+    
 
 def get_comic_number():
     url = 'https://xkcd.com/info.0.json'
@@ -19,8 +19,8 @@ def get_comic_number():
     comic_info = response.json()
     number = comic_info['num']
     return number
+
 def save_comic(random_comic):
-    
     url = f'https://xkcd.com/{random_comic}/info.0.json'
     response = requests.get(url)
     response.raise_for_status()
@@ -29,7 +29,8 @@ def save_comic(random_comic):
     alt = comic_info['alt']
     title = comic_info['title']
     return png_url, alt, title 
-def upload_files(bot_api_key, chat_id, title, alt):
+
+def upload_file(bot_api_key, chat_id, title, alt):
     filename = f'{title}.png'
     bot = telegram.Bot(token=bot_api_key)
     with open(filename, 'rb') as file:
@@ -40,13 +41,15 @@ def upload_files(bot_api_key, chat_id, title, alt):
 
 def main():
     load_dotenv()
-    random_comic = random.randint(1, get_comic_number())
     bot_api_key = os.environ['TG_BOT_API']
     chat_id = os.environ['TG_CHAT_ID']
-    png_url, alt, title = save_comic(random_comic)
-    filename = upload_files(bot_api_key, chat_id, title, alt)
-    download_comic(f'{title}.png', png_url)
-    os.remove(filename)
+    try:
+        random_comic = random.randint(1, get_comic_number())
+        png_url, alt, title = save_comic(random_comic)
+        download_comic(f'{title}.png', png_url)
+        filename = upload_file(bot_api_key, chat_id, title, alt)
+    finally:
+        os.remove(filename)
 
 
 if __name__ == '__main__':
